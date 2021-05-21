b2b4f574ffe93850962aed6cbfac3806e3250710
ChristoherHenao <cq.henao@gmail.com>
2021-05-21 13:04:52 -0400

    project set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom';
+import Home from './Home'
+import Form from './Form'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Link to="/"><button>Home</button></Link>
+      <Link to="/pizza"><button>Order Pizza</button></Link>
+
+
+      
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/pizza" component={Form}/>
     </>
   );
 };
