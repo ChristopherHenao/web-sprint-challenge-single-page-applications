6b4f1143062da5d7a2e5b5fd21f3bbd4ed879baa
ChristoherHenao <cq.henao@gmail.com>
2021-06-18 15:13:57 -0400

    form bug fixed

--- src/Form.js
+++ src/Form.js
@@ -23,6 +23,36 @@ const Form = () => {
                         <option value='xLarge'>X-Large</option>
                     </select><br/>
 
+                <label>Select Toppings: </label><br/>
+                    <label> Pepperoni </label>
+                        <input 
+                        name='pepperoni-topping1'
+                        type='checkbox'
+                        value='pepperoni'
+                        />
+
+                    <label> Chicken </label>
+                        <input 
+                        name='chicken-topping2'
+                        type='checkbox'
+                        value='chicken'
+                        />
+
+                    <label> Ham </label>
+                        <input 
+                        name='ham-topping3'
+                        type='checkbox'
+                        value='ham'
+                        />      
+
+                    <label> Pineapple </label>
+                        <input 
+                        name='pineapple-topping1'
+                        type='checkbox'
+                        value='pineapple'
+                        />
+                <br/>
+
                 <label>Special Instructions: </label>
                     <input 
                     id='special-text'
