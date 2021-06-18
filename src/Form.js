435e83579fb23ee2e34b53052dcc3a8d1f0d0b06
ChristoherHenao <cq.henao@gmail.com>
2021-06-18 14:54:07 -0400

    form structure done

--- src/Form.js
+++ src/Form.js
@@ -6,18 +6,29 @@ const Form = () => {
         <div>
             <form id='pizza-form'>
                 <label>Name: </label>
-                <input 
-                id='name-input'
-                name='name'
-                type='text'
-                /><br/>
+                    <input 
+                    id='name-input'
+                    name='name'
+                    type='text'
+                    /><br/>
+                
+                <label>Pizza Size: </label>
+                    <select 
+                    name='size'
+                    id='size-dropdown'
+                    >
+                        <option value=''>Select a size</option>
+                        <option value='medium'>Medium</option>
+                        <option value='large'>Large</option>
+                        <option value='xLarge'>X-Large</option>
+                    </select><br/>
 
                 <label>Special Instructions: </label>
-                <input 
-                id='special-text'
-                name='specialInstructions'
-                type='text'
-                />
+                    <input 
+                    id='special-text'
+                    name='specialInstructions'
+                    type='text'
+                    />
             </form>
         </div>
     )
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,21 @@
 import React from "react"
+import { useHistory } from 'react-router-dom'
 
 const HomePage = () => {
 
+    const history = useHistory()
+
+    const routeToForm = () => {
+        history.push('/pizza')
+    }
+
     return (
         <div>
-            HomePage
+            <div>
+                {/* img is set here */}
+            </div>
+            <img src='../Assets/Pizza.jpg' alt='pizza on a dish'/><br/>
+            <button id='order-pizza' onClick={routeToForm}>Pizza?</button>
         </div>
     )
 }
